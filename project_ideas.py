a2cb351ecf9fe5054a6efc735adcb368c4d691bc
Liam McMahon <mcmahonliam837@gmail.com>
2020-03-31 00:37:18 -0400

    1.0

--- project_ideas.py
+++ project_ideas.py
@@ -3,6 +3,7 @@
 import argparse
 import sys
 import os
+import editor
 
 projFile = "./project_ideas.txt"
 
@@ -20,10 +21,10 @@ def getProjData(projName):
 def desc(projName):
     (name, descFile) = getProjData(projName)
     if name is None or descFile is None:
-        print('The project {0} was not found, check: project_ideas --list')
+        print('The project {0} was not found, check: project_ideas list')
         exit(-1)
 
-    if not os.path.exists('desc/{0}'.format(descFile)):
+    if not os.path.exists('outline/{0}'.format(descFile)):
         _ = createProjDesc(name)
 
     with open(descFile) as f:
@@ -41,28 +42,30 @@ def newProjIdea(name):
     if len(name) < 3:
         print('Invalid name')
         exit(-1)
-    desc = createProjDesc(name)
+    outline = createProjDesc(name)
     with open(projFile, 'a') as f:
-        f.write("{0},{1}\n".format(name, desc))
+        f.write("{0},{1}\n".format(name, outline))
+
+    editor.edit(outline)
 
 def createProjDesc(name):
-    descPath = 'desc/' + name.replace(' ', '_').split('.')[0] + '_outline' + '.txt'
-    with open('desc/' + name.replace(' ', '_').split('.')[0] + '_outline' + '.txt', 'w') as f:
+    descPath = 'outline/' + name.replace(' ', '_').split('.')[0] + '_outline' + '.txt'
+    with open('outline/' + name.replace(' ', '_').split('.')[0] + '_outline' + '.txt', 'w') as f:
         f.write(name)
     return descPath
 
 def parseArgs():
     parser = argparse.ArgumentParser(description='A data-bank of project ideas')
-    parser.add_argument('--list', action='store_true', help='Lists all project ideas')
-    parser.add_argument('--outline', help='Gets project outline by name')
-    parser.add_argument('--new', help='Creates new project idea')
+    parser.add_argument('-s', '--show', required=False, action='store_true', help='Lists all project ideas')
+    parser.add_argument('-o', '--outline', required=False, help='Gets project outline by name')
+    parser.add_argument('-n', '--new', required=False, help='Creates new project idea')
     return parser.parse_args()
 
 
 def main():
     args = parseArgs()
 
-    if args.list:
+    if args.show != None:
         listProjs()
     elif args.outline != None:
         desc(args.outline.strip())
